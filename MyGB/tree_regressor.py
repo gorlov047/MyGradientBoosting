96b882fad57002b13985a7714e2c9a5a1c7d706a
gorlov047 <gorlov047@gmail.com>
2023-09-19 11:40:27 +0300

    add method for find best split

--- MyGB/tree_regressor.py
+++ MyGB/tree_regressor.py
@@ -39,14 +39,33 @@ class DecisionTreeRegressor():
                          feature_vector: np.ndarray,
                          target_vector:np.ndarray
                          )-> Tuple[np.ndarray, np.ndarray, np.number, np.number]:
-        pass
+        def variance(sum_, sum_of_squares, number):
+            return sum_of_squares / number - (sum_ / number) ** 2
+
+        sorted_inds = np.argsort(feature_vector)
+        sorted_feature, inds = np.unique(feature_vector[sorted_inds],
+                                         return_index=True)
+        tresholds = (sorted_feature[1:] + sorted_feature[:-1]) / 2
+        left_sum = np.cumsum(target_vector[sorted_inds])[inds - 1][1:]
+        left_sq_sum = np.cumsum(target_vector[sorted_inds] ** 2)[inds - 1][1:]
+        left_count = np.arange(1, feature_vector.shape[0], 1)[inds - 1][1:]
+        left_var = variance(left_sum, left_sq_sum, left_count)
+
+        right_count = feature_vector.shape[0] - left_count
+        right_var = variance(target_vector.sum() - left_sum,
+                             (target_vector ** 2).sum() - left_sq_sum,
+                             right_count)
+
+        info_gain = -((left_count * left_var + right_count * right_var) / feature_vector.shape[0])
+        best_ind = np.argmax(info_gain)
+        return tresholds, info_gain, tresholds[best_ind], info_gain[best_ind]
 
     def _fit_node(self, sub_x: np.ndarray, sub_y: np.ndarray, node: dict) -> None:
         pass
-        
+  
     def _predict_node(self, sample: np.ndarray, node: dict) -> float:
         pass
- 
+
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """fit model"""
         pass
